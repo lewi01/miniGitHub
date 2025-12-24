e841a7348b83ac39c0a8c9b7637b7563809e8aca
lewi01 <isslewi03@gmail.com>
2025-12-24 15:14:02 +0300

    liquibase changesets

---
+++ GithubMini/src/main/java/com/lewiscode/githubmini/GithubMiniApplication.java
@@ -0,0 +1,13 @@
+package com.lewiscode.githubmini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubMiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubMiniApplication.class, args);
+	}
+
+}
